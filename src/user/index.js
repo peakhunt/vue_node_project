f0ecdae8078e0accfa61c4c3d546e22a62f81c04
Hawk Kim <hawk.kim1974@gmail.com>
2019-01-18 16:10:19 +0900

    added more user functionality and test cases

--- src/user/index.js
+++ src/user/index.js
@@ -1,28 +1,119 @@
 const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
 const config = require('../config');
 const logger = require('../logger');
 
+const logged_in_users = {
+};
+
+function get_login_info(id) {
+  return logged_in_users[id];
+}
+
+function login_user(user) {
+  logger.info(`${user.id} logged in`);
+
+  const { data: { user_mgmt: { super_secret: secret } } } = config;
+  const uinfo = {
+    user,
+    access_time: moment()
+  };
+
+  const token = jwt.sign(uinfo, secret);
+
+  logged_in_users[user.id] = {
+    uinfo,
+    token
+  };
+
+  return token;
+}
+
 function login(id, password) {
-  if (id == null || id == undefined || password == null | password == undefined) {
-    return false;
+  if (id === null || id === undefined || password === null || password === undefined) {
+    return { status: false, token: undefined };
   }
 
-  const { data: { user: users } } = config;
-  const csum = crypto.createHash('sha256').update(password, 'utf8').digest('hex');
+  const { data: { user_mgmt: { users } } } = config;
 
+  const csum = crypto.createHash('sha256').update(password, 'utf8').digest('hex');
 
   for (let i = 0; i < users.length; i += 1) {
     const user = users[i];
 
     if (user.id === id && user.password === csum) {
-      logger.info(`${user.id} logged in`);
-      return true;
+      return {
+        status: true,
+        token: login_user(user)
+      };
     }
   }
   logger.info(`${id} failed to login`);
-  return false;
+  return { status: false, token: undefined };
+}
+
+function logout(id) {
+  const linfo = logged_in_users[id];
+
+  if (linfo === undefined) {
+    logger.debug(`logging out ${id} is not logged in`);
+    return;
+  }
+
+  logged_in_users[id] = undefined;
+  logger.info(`${id} logged out`);
+}
+
+function authorize(id, token) {
+  const linfo = logged_in_users[id];
+  const now = moment();
+
+  if (linfo === undefined) {
+    logger.debug(`${id} not authorized`);
+    return false;
+  }
+
+  if (linfo.token !== token) {
+    logger.warn(`token mismatch for ${id}`);
+    return false;
+  }
+
+  const { data: { user_mgmt: { timeout_in_secs: timeout } } } = config;
+  const diff = now.diff(linfo.uinfo.access_time, 'seconds');
+
+  if (diff > timeout) {
+    logger.debug(`${id} timedout`);
+    logout(id);
+    return false;
+  }
+
+  linfo.uinfo.access_time = now;
+
+  return true;
+}
+
+function decode(token, cb) {
+  if (token === null || token === undefined) {
+    process.nextTick(() => {
+      cb('invalid argument');
+    });
+    return;
+  }
+
+  const { data: { user_mgmt: { super_secret: secret } } } = config;
+
+  jwt.verify(token, secret, (err, decoded) => {
+    cb(err, decoded);
+  });
 }
 
 module.exports = {
-  login
+  login,
+  logout,
+  authorize,
+  decode,
+  _private_for_test: {
+    get_login_info
+  }
 };
--- test/user_test.js
+++ test/user_test.js
@@ -1,5 +1,7 @@
 import * as assert from 'assert';
+const moment = require('moment');
 const user = require('../src/user');
+const config = require('../src/config');
 
 const loginTestSamples = [
   { id: 'admin', password: 'password', expectedResult: true, description: 'admin should be logged in' },
@@ -16,9 +18,111 @@ const loginTestSamples = [
 ];
 
 describe('user module', () => {
-  loginTestSamples.forEach((sample) => {
-    it(sample.description, () => {
-      assert.equal(user.login(sample.id, sample.password), sample.expectedResult);
+  describe('login', () => {
+    loginTestSamples.forEach((sample) => {
+      it(sample.description, () => {
+        assert.equal(user.login(sample.id, sample.password).status, sample.expectedResult);
+      });
+    });
+  });
+
+  describe('authorize - token', () => {
+    assert.equal(user.login('admin', 'password').status, true);
+
+    var linfo = user._private_for_test.get_login_info('admin');
+
+    assert.equal(user.authorize('admin', null), false);
+    assert.equal(user.authorize('admin', undefined), false);
+    assert.equal(user.authorize('admin', linfo.token), true);
+
+  });
+
+  describe('logout', () => {
+    it('normal logout', () => {
+      assert.equal(user.login('admin', 'password').status, true);
+
+      var linfo = user._private_for_test.get_login_info('admin');
+      assert.equal(linfo !== undefined, true);
+
+      assert.equal(user.authorize('admin', linfo.token), true);
+
+      user.logout('admin');
+
+      linfo = user._private_for_test.get_login_info('admin');
+
+      assert.equal(linfo, undefined);
+
+      assert.equal(user.authorize('admin'), false);
+    });
+
+    it('logout when not logged in', () => {
+      assert.equal(user.authorize('admin'), false);
+      user.logout('admin');
+      assert.equal(user.authorize('admin'), false);
+    });
+
+    it('login timeout', () => {
+      assert.equal(user.login('admin', 'password').status, true);
+
+      var linfo = user._private_for_test.get_login_info('admin');
+      assert.equal(linfo !== undefined, true);
+
+      assert.equal(user.authorize('admin', linfo.token), true);
+
+      const { data: { user_mgmt: { timeout_in_secs: timeout } } } = config;
+
+      const now = moment();
+      linfo.uinfo.access_time.subtract(timeout + 10, 'seconds');
+
+      assert.equal(user.authorize('admin', linfo.token), false);
+
+      linfo = user._private_for_test.get_login_info('admin');
+      assert.equal(linfo === undefined, true);
+    });
+  });
+
+  it('decode test - normal', (done) => {
+    assert.equal(user.login('admin', 'password').status, true);
+    var linfo = user._private_for_test.get_login_info('admin');
+
+    user.decode(linfo.token, (err, decoded) => {
+      if(err) {
+        done(err);
+      } else {
+        if(decoded.token === linfo.token && linfo.user.id === 'admin') {
+          done();
+        } else {
+          done(err);
+        }
+      }
+    });
+  });
+
+  it('decode test - fail1', (done) => {
+    user.decode(null, (err, decoded) => {
+      if(err) {
+        done();
+      } else {
+        if(decoded.token === linfo.token) {
+          done(err);
+        } else {
+          done(err);
+        }
+      }
+    });
+  });
+
+  it('decode test - fail2', (done) => {
+    user.decode(null, (err, decoded) => {
+      if(err) {
+        done();
+      } else {
+        if(decoded.token === linfo.token) {
+          done(err);
+        } else {
+          done(err);
+        }
+      }
     });
   });
 });
