42f0aadd66087ef94b58de3895fb1fb53341b31a
Hawk Kim <hawk.kim1974@gmail.com>
2019-01-21 10:06:03 +0900

    adding vuetify template for ui development

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,15 +1,16 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import About from './views/About.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: Home
     },
     {
       path: '/about',
@@ -17,7 +18,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
-});
+      component: About
+    }
+  ]
+})
